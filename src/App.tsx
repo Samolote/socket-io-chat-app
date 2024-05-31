3e96de465cb6ed399b750e09385d23052766479e
Wojciech Łukowski <wojtek304@gmail.com>
2024-05-31 19:12:11 +0200

    Add layout for chatroom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import './App.scss';
 
 const App = () => {
-  return <>app</>;
+  return (
+    <main className="container">
+      <header>
+        <h2>websockets chat app</h2>
+      </header>
+      <ul className="messages-list">
+        <li className="messages-list__element messages-list__element--incoming">
+          <article>Incomming</article>
+        </li>
+        <li className="messages-list__element messages-list__element--outgoing">
+          <article>Outgoing</article>
+        </li>
+      </ul>
+      <form>
+        <fieldset role="group">
+          <input type="text" />
+          <button>Send</button>
+        </fieldset>
+      </form>
+    </main>
+  );
 };
 
 export default App;
