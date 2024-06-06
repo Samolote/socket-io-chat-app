4a55de8df2d1907be3fc33eb318cb0ce96e54233
Wojciech Łukowski <wojtek304@gmail.com>
2024-06-06 22:12:38 +0200

    Add consts file with socket.io events

---
+++ src/SOCKET_IO_EVENTS.ts
@@ -0,0 +1,9 @@
+export const SOCKET_IO_EVENTS = {
+  CONNECT: 'connection',
+  DISCONNECT: 'disconnect',
+  CREATE_CHANNEL: 'createChannel',
+  REMOVE_CHANNEL: 'removeChannel',
+  JOIN_CHANNEL: 'joinChannel',
+  LEAVE_CHANNEL: 'leaveChannel',
+  SEND_MESSAGE: 'sendMessage',
+};
