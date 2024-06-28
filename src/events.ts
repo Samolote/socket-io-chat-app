0ebfa8c9296e8079d2617a23ed3eda610ad4c3ef
Wojciech Łukowski <wojtek304@gmail.com>
2024-06-28 17:11:41 +0200

    Add types for SocketIo specific events

--- src/events.ts
+++ src/events.ts
@@ -1,6 +1,7 @@
 export enum SocketIoEvents {
   CONNECT = 'connection',
   DISCONNECT = 'disconnect',
+  CONNECT_ERROR = 'connectError',
   CREATE_CHANNEL = 'createChannel',
   REMOVE_CHANNEL = 'removeChannel',
   JOIN_CHANNEL = 'joinChannel',
@@ -8,3 +9,27 @@ export enum SocketIoEvents {
   SEND_MESSAGE = 'sendMessage',
   BROADCAST_MESSAGE = 'broadcastMessage',
 }
+
+type MessagePayloadType = {
+  message: string;
+  socketId: string;
+  messageId: string;
+};
+
+export type ServerToClientEvents = {
+  [SocketIoEvents.BROADCAST_MESSAGE]: (payload: MessagePayloadType) => void;
+  [SocketIoEvents.CONNECT_ERROR]: (error: { message: string }) => void;
+};
+
+export type ClientToServerEvents = {
+  [SocketIoEvents.SEND_MESSAGE]: (payload: MessagePayloadType) => void;
+};
+
+/**
+ * @description
+ * this type is a placeholder as it has no use as of now
+ * but it has to be passed while passing generics into server instance
+ */
+export type InterServerEvents = {
+  ping: () => void;
+};
