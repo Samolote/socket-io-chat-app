9b4f2c40d4edc7c89e293c9ba7fd38bcefdc36ac
Wojciech Łukowski <wojtek304@gmail.com>
2024-06-28 17:12:34 +0200

    Add types for socket.io-client instance

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,11 @@
-import { io } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
+import { ServerToClientEvents, ClientToServerEvents } from './events';
+
+export type SocketData = {
+  username: string;
+};
 
 const URL = 'http://localhost:8080';
-const socket = io(URL, { autoConnect: false });
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(URL, { autoConnect: false });
 
 export default socket;
